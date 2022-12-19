fbd4fea77d453045403617a3e2162cd8eeaf368a
jiaqianglim <jqlimjiaqiang@gmail.com>
2022-12-19 14:40:34 +0800

    MONGODB

--- src/main/java/vttp/persistence/repository/MONGODB_TEXT_Repository.java
+++ src/main/java/vttp/persistence/repository/MONGODB_TEXT_Repository.java
@@ -15,21 +15,52 @@ public class MONGODB_TEXT_Repository {
     private MongoTemplate mongoTemp;
 
     /*
-    matchingAny(String... words) - match any of the given words, bag of words
-    matchingPhrase(String phrase) - match a phrase
-    notMatchingAny(String... words) - not matching any of the given words
-    notMatchingPhrase(String phrase) - do not match the phrase
+     * matchingAny(String... words) - match any of the given words, bag of words
+     * matchingPhrase(String phrase) - match a phrase
+     * notMatchingAny(String... words) - not matching any of the given words
+     * notMatchingPhrase(String phrase) - do not match the phrase
      */
 
-    public List<Document> getPHObjBySearchTerm(String searchTerm){
-        TextCriteria textCriteria = TextCriteria.forDefaultLanguage().matchingPhrase(searchTerm);
-        // TextCriteria textCriteria = TextCriteria.forDefaultLanguage().matchingPhrase(searchTerm).caseSensitive(true);
+    public List<Document> getPHObjBySearchTerm(TextCriteria textCriteria) {
+        // TextCriteria textCriteria =
+        // TextCriteria.forDefaultLanguage().matchingPhrase(searchTerm).caseSensitive(true);
         TextQuery query = TextQuery.queryText(textCriteria);
         List<Document> results = mongoTemp.find(query, Document.class, "collectionname");
         return results;
     }
 
-    public List<Document> getPHObjBySearchString(String searchString){
-        return null;
+    public List<Document> getPHObjBySearchTermSortByScore(TextCriteria textCriteria) {
+        // TextCriteria textCriteria =
+        // TextCriteria.forDefaultLanguage().matchingPhrase(searchTerm).caseSensitive(true);
+        TextQuery query = TextQuery.queryText(textCriteria).sortByScore();
+        query.setScoreFieldName("score");
+        List<Document> results = mongoTemp.find(query, Document.class, "collectionname");
+        return results;
+    }
+
+    public TextCriteria createCriteriaBySearchTerm(String searchTerm) {
+        TextCriteria textCriteria = TextCriteria.forDefaultLanguage().matchingPhrase(searchTerm);
+        return textCriteria;
+    }
+
+    public TextCriteria createCriteriaBySearchString(String searchString) {
+        StringBuilder matchesSB = new StringBuilder();
+        StringBuilder notMatchSB = new StringBuilder();
+        for (String word : searchString.split(",")) {
+            String w = word.trim();
+            if (w.startsWith("-")) {
+                notMatchSB.append(" ");
+                notMatchSB.append(w.substring(-1));
+            } else {
+                matchesSB.append(" ");
+                matchesSB.append(w);
+            }
+        }
+        String matches = matchesSB.toString();
+        String notMatch = notMatchSB.toString();
+
+        TextCriteria textCriteria = TextCriteria.forDefaultLanguage().matchingAny(matches).notMatchingAny(notMatch);
+
+        return textCriteria;
     }
 }
